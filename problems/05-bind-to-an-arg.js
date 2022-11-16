3a8ca436412a58746e7d77df9f91af73be161ebf
Vusal Layijov <Layijovvusal@gmail.com>
2022-11-16 15:17:57 -0800

    SOn

--- problems/05-bind-to-an-arg.js
+++ problems/05-bind-to-an-arg.js
@@ -1,6 +1,9 @@
 function bindToAnArg(func, arg) {
   // Your code here
+  let binded = func.bind(func, arg)
+  console.log(binded)
+  return binded
 }
-
+  
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 module.exports = bindToAnArg;
